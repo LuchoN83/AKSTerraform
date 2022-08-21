5e59048ea47b4f1ce6dad0f1737a6b532a4b75a2
LuchoN83 <39603304+LuchoN83@users.noreply.github.com>
2022-08-21 13:54:41 -0500

    Create variables.tf

---
+++ ACR/variables.tf
@@ -0,0 +1,21 @@
+variable "keyvault_rg" {
+  type = string
+}
+variable "keyvault_name" {
+  type = string
+}
+
+variable "clientidkvsecret" {
+  type = string
+}
+
+variable "acrname" {
+  type = string
+}
+
+variable "rg_name" {
+  type = string
+}
+variable "rg_location" {
+  type = string
+}
