a7ff1fa2bd5f215e505c33a08901b65a6dd4e097
LuchoN83 <39603304+LuchoN83@users.noreply.github.com>
2022-08-21 13:30:52 -0500

    Update main.tf

--- ACR/main.tf
+++ ACR/main.tf
@@ -1,1 +1,30 @@
+data "azurerm_key_vault" "azure_vault" {
+  name = var.keyvault_name
+  resource_group_name = var. keyvault_rg
+}
+
+data "azurerm_key_vault_secret" "spn_id" {
+  name = var.clientidkvsecret
+  key_vault_id = data.azurerm_key_vault.azure_vaulr.id
+ }
+
+data "azuread_service_principal" "aks_principal"{
+  application_id = data.azurerm_key_vault_secret.spn_id.value
+}
+
+resource "azurerm_container_registry" "acr" {
+  name = var.acrname
+  resource_group_name = var.rg_name
+  location = var.rg_location
+  sku = "Basic"
+  admin_enabled = true
+}
+
+resource "azurerm_role_assigment" "acrpull_role" {
+  scope = azurerm_container_registry.acr.ed
+  role_definition_name = "AcrPull"
+  principal_id = data.azuread_service_principal.aks_principal.id
+  skip_service_principal_aad_check = true
+}
+
 
